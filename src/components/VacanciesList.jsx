dde3ca87b8532b06a47d5e50fb86c7b46bc614a5
Pawel Makarewicz <pawelmakarewicz.1996@gmail.com>
2023-05-20 21:27:14 +0200

    fix eslint config

--- src/components/VacanciesList.jsx
+++ src/components/VacanciesList.jsx
@@ -3,24 +3,23 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getVacancies } from '../slices/vacanciesSlice';
 
 const useInitialVacancies = () => {
-    const dispatch = useDispatch();
-    const accessTokenLoadingStatus = useSelector(state => state.accessTokens.loadingStatus);
+  const dispatch = useDispatch();
+  const accessTokenLoadingStatus = useSelector((state) => state.accessTokens.loadingStatus);
 
-
-    useEffect(() => {
-        if (accessTokenLoadingStatus === 'succeed') {
-            dispatch(getVacancies());
-        }
-    }, [accessTokenLoadingStatus]);
-}
+  useEffect(() => {
+    if (accessTokenLoadingStatus === 'succeed') {
+      dispatch(getVacancies());
+    }
+  }, [accessTokenLoadingStatus]);
+};
 
 function VacanciesList(props) {
-    useInitialVacancies();
+  useInitialVacancies();
 
-    // TODO: subscribe to vacansies and render
-    // useSelector(state => state.vacancies);
-    
-    return <div>TODO: render vacancies</div>
+  // TODO: subscribe to vacansies and render
+  // useSelector(state => state.vacancies);
+
+  return <div>TODO: render vacancies</div>;
 }
 
 export default VacanciesList;
