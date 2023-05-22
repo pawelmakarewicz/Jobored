74e4a9ecbbe78a7aa1211ae62519c0b61c80488e
Pawel Makarewicz <pawelmakarewicz.1996@gmail.com>
2023-05-22 13:35:21 +0200

    fix salry Filter

--- src/api/queryParams/searchParameters.js
+++ src/api/queryParams/searchParameters.js
@@ -1,10 +1,16 @@
 import { snakeCase } from 'lodash';
 
+function isSalaryParamsPresent(params) {
+  return Object.prototype.hasOwnProperty.call(params, 'payment_from') || Object.prototype.hasOwnProperty.call(params, 'payment_to');
+}
+
 function setSearchParamsApi(params) {
-  // eslint-disable-next-line no-unused-vars
-  const object = Object.fromEntries(Object.entries(params).filter(([_, v]) => v).map(([key, value]) => [snakeCase(key), value]));
-  console.log(object);
-  return object;
+  // eslint-disable-next-line no-unused-vars, max-len
+  const apiParams = Object.fromEntries(Object.entries(params).filter(([_, v]) => v).map(([key, value]) => [snakeCase(key), value]));
+  if (isSalaryParamsPresent(apiParams)) {
+    return { no_agreement: 1, ...apiParams };
+  }
+  return apiParams;
 }
 
 function initSearchParams(searchParamsFromFilters) {
